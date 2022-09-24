f0c5fd521205f391003e429260b95770411f1b4a
sh1ft <31477929+Federico-Shih@users.noreply.github.com>
2022-09-24 17:03:01 -0300

    Creadas las paginas y routeo

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Landing from "../views/LandingPage.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: import.meta.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "landing",
+      component: Landing,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/home",
+      name: "home",
+      component: () => import("../views/HomePage.vue"),
+      children: [
+        {
+          path: "explore",
+          name: "explore",
+          component: () => import("../views/ExplorePage.vue"),
+        },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
