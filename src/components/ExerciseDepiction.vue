1126b02c660b3c81ddf33dd89092dae72551d022
Mauro Báez <mabaez@itba.edu.ar>
2022-09-28 18:54:19 -0300

    ExerciseDepiction

---
+++ src/components/ExerciseDepiction.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card
+    shaped
+    class="community-profile-long align-end flex flex-row"
+    @mouseenter="hover = true"
+    @mouseleave="hover = false"
+    :elevation="hover ? 5 : 2"
+    :class="variants.boxSizes[variant]"
+  >
+    <v-list-item>
+      <v-list-item-content>
+        <div class="d-flex flex-row">
+          <img :src="this.picture" :alt="this.picture" width="150px" height="55px" class="mr-4"/>
+          <div>
+            <v-list-item-title class="font-weight-bold">
+              {{ this.exerciseName }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ this.category }}
+            </v-list-item-subtitle>
+          </div>
+        </div>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ExerciseDepiction",
+  props: {
+    variant: String,
+    exerciseName: String,
+    category: String,
+    picture: String,
+    profileId: String,
+  },
+  data(){
+    return{
+      hover: false,
+      variants:{
+        boxSizes: {
+          large: "community-profile-button-large",
+          small: "community-profile-button-small"
+        },
+        fontSizes: {
+          large: "community-profile-font-large",
+          small: "community-profile-font-small",
+        },
+      }
+
+    }
+  }
+
+};
+
+</script>
+<style scoped>
+
+.community-profile-button-large{
+  width: 550px;
+  height: 100px;
+  margin: 10px 5px;
+}
+.community-profile-button-small{
+  width: 400px;
+  height: 75px;
+  margin:10px 5px;
+
+}
+.community-profile-font-large{
+  font-size: 1.8em;
+}
+.community-profile-font-small{
+  font-size: 1.2em;
+}
+</style>
--- src/views/ExercisePage.vue
+++ src/views/ExercisePage.vue
@@ -2,6 +2,7 @@
   <v-container>
     <v-row>
       <v-col>
+        <ExerciseDepiction variant="small" :picture="abdominales" exercise-name="Short Abs" category="Abdominales" />
         <h1>Todos los Ejercicios</h1>
       </v-col>
       <v-spacer>|</v-spacer>
@@ -13,8 +14,16 @@
 </template>
 
 <script>
+import ExerciseDepiction from "@/components/ExerciseDepiction.vue";
+import abspic from "../assets/abdominales.jpg";
 export default {
-  name: "ExercisePage.vue"
+  name: "ExercisePage.vue",
+  components: { ExerciseDepiction },
+  data() {
+    return {
+      abdominales: abspic,
+    }
+  },
 };
 </script>
 
