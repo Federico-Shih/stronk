eda02f4e9b9d2e51ce42456d2bfbb4f85af9fefb
fsh1ft <31477929+Federico-Shih@users.noreply.github.com>
2022-09-29 14:59:40 -0300

    Iniciado la creacion de cards de rutina

---
+++ src/components/rutine/CycleCard.vue
@@ -0,0 +1,31 @@
+<script>
+class Exercise {
+  constructor(id, title, img_url, reps, duration) {
+    this.id = id;
+    this.title = title;
+    this.img_url = img_url;
+    this.reps = reps;
+    this.duration = duration;
+  }
+}
+
+export default {
+  props: {
+    title: String,
+    repetitions: Number,
+    exercises: Array,
+  },
+  name: "CycleCard",
+};
+</script>
+<template>
+  <v-card>
+    <v-card-title>{{ this.title }}</v-card-title>
+    <div v-for="exercise in exercises" :key="exercise.id">
+      <div>
+        {{ exercise.title }}
+      </div>
+      <div><v-img width="200" :src="exercise.img_url"></v-img></div>
+    </div>
+  </v-card>
+</template>
--- src/views/RoutinePage.vue
+++ src/views/RoutinePage.vue
@@ -1,11 +1,22 @@
 <script>
+import abdominales from "../assets/abdominales.jpg";
+import CycleCard from "../components/rutine/CycleCard.vue";
 export default {
   mounted() {
     console.log(this.$route.params.id);
   },
+  components: { CycleCard },
+  data: () => ({
+    img: abdominales,
+  }),
 };
 </script>
 
 <template>
-  <div></div>
+  <div style="width: 500px">
+    <CycleCard
+      title="Hola"
+      :exercises="[{ img_url: img, title: 'hola' }]"
+    ></CycleCard>
+  </div>
 </template>
