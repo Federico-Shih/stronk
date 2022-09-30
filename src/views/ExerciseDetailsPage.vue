742996aa75dc56796645e318995c6e4de464b3df
RupnikF <francorupnik@gmail.com>
2022-09-30 20:51:17 -0300

    Categorias Exercise View

--- src/views/ExerciseDetailsPage.vue
+++ src/views/ExerciseDetailsPage.vue
@@ -18,22 +18,25 @@
 
     </div>
 
-    <div class="ml-6 mb-4 d-flex flex-row justify-space-between">
-      <div class="d-flex flex-row justify-start ">
+    <div class="ml-6 mb-4 d-flex flex-row ">
+      <div class="d-flex flex-row justify-self-start">
         <v-btn icon class="mx-4">
           <v-img class="rounded-circle" :src="temp" height="50px" width="50px"/>
         </v-btn>
         <h3 class="mt-1">hecho por {{ creatorname }}</h3>
       </div>
-      <!--        <div>-->
-      <!--          aca irian las categorias si supiera como hacerlas en la forma que esta, pero serian botones en column -->
-      <!--        </div>-->
-
+      <div class="d-flex flex-row offset-1 mr-6 mt-1 justify-space-between">
+        <h3 class="pr-4">Categorias:</h3>
+        <template v-for="index in category.length" :id="index">
+          <v-btn x-small class="rounded-l-pill mt-1 mr-5">{{category[index-1]}}</v-btn>
+        </template>
+      </div>
     </div>
     <div class="d-flex flex-column ml-8 mb-8" style="width: 70%">
       <h2 class="mb-2">Descripcion del Ejercicio</h2>
       <p>{{ description }}</p>
     </div>
+    
 
 
   </div>
@@ -52,7 +55,8 @@ export default {
     return {
       temp,
       creatorname: 'Arnold Schwarzenegger',
-      description: 'Esta es la descripcion Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a nibh vitae nisi tincidunt vulputate vel nec risus. Fusce placerat sagittis nisl, quis ultricies quam scelerisque a. Nulla laoreet tellus a turpis hendrerit posuere. Curabitur in ante velit. Mauris eu dolor tortor. Curabitur nisl velit, tincidunt non orci id, rutrum sodales metus. Fusce vitae libero aliquet lorem mollis vestibulum nec non odio. Fusce placerat egestas dui at venenatis. Aliquam aliquet orci elit, ut aliquet justo mattis et. Curabitur vitae iaculis neque, ac eleifend nibh. Nam volutpat tortor sed leo sollicitudin, eget semper quam ultricies'
+      description: 'Esta es la descripcion Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a nibh vitae nisi tincidunt vulputate vel nec risus. Fusce placerat sagittis nisl, quis ultricies quam scelerisque a. Nulla laoreet tellus a turpis hendrerit posuere. Curabitur in ante velit. Mauris eu dolor tortor. Curabitur nisl velit, tincidunt non orci id, rutrum sodales metus. Fusce vitae libero aliquet lorem mollis vestibulum nec non odio. Fusce placerat egestas dui at venenatis. Aliquam aliquet orci elit, ut aliquet justo mattis et. Curabitur vitae iaculis neque, ac eleifend nibh. Nam volutpat tortor sed leo sollicitudin, eget semper quam ultricies',
+      category:['Musculo Grande','Musculo Chico','Dificultad'],
     }
   },
 };
